085233a8c1235861bcfa2aa08598c923b192b349
Sagar saini <sagarsaini954818@gmail.com>
2025-10-26 12:14:36 +0530

    ss

--- js everything/script.js
+++ js everything/script.js
@@ -4,11 +4,11 @@
 // Temporal dead zone => utna area jitne mein js kp pata ahi ki variable exist karta hai par wo appko value nahi de sakta
 // hoisting
 // hoisting impact per type
-var a = 12;
+// 
 
-let b = 123;
+console.log("SAGAR")
 
-console.log(d)
-
-
-let d;
+for (let index = 0; index < 15; index++) {
+    console.log(index)
+    
+}
