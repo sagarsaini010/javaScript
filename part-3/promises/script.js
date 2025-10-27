63a13b397bad08c71841b1bd12e5a6e6676cbafa
Sagar saini <sagarsaini954818@gmail.com>
2025-10-27 14:07:50 +0530

    promises

--- part-3/promises/script.js
+++ part-3/promises/script.js
@@ -1,14 +1,21 @@
 let pr = new Promise(function(resolve, reject){
+    setTimeout(() => {
        let a = Math.floor(Math.random() * 10);
        if(a % 2 == 0){
            resolve("Even number and Promise resolved: " + a);
        } else {
            reject("Odd number and Promise rejected: " + a);
        }
+   }, 2000);
 });
 
-pr.then(function(message){
-    console.log(message);
-}).catch(function(message){
-    console.log(message);
-});
+async function abcd() {
+    try {
+        let val = await pr;
+        console.log(val);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+abcd();
