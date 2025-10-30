f7e24077007748ea0b1ed9d25adf01f79686227e
Sagar saini <sagarsaini954818@gmail.com>
2025-10-30 11:05:51 +0530

    callback hell

--- DOM/script.js
+++ DOM/script.js
@@ -1,6 +1,29 @@
-let h1 = document.createElement('h1')
-h1.innerText = "Hello bro"
+function profile(username, cb) {
+  setTimeout(() => {
+    cb({ _id: 1122, username, age: 26, email: "Sagarsaini@gmail.com" });
+  }, 2000);
+}
 
-console.log(h1)
+function postLekraao(id,cb){
+    setTimeout(() => {
+        cb({_id: id, posts:['Hello','good','morning']})
+    }, 3000);
+}
 
-document.body.prepend(h1)
+function savedpostlekraao(id,cb){
+    setTimeout(() => {
+        cb({_id:id,savedPost: ["!212",'121','3212','3123']})
+    }, 2000);
+}
+
+profile("Sagar Saini", function (data) {
+    
+  postLekraao(data._id, function (posts) {
+
+    savedpostlekraao(data._id, function(saved){
+        console.log(data)
+        console.log(posts)
+        console.log(saved)
+    })
+  });
+});
